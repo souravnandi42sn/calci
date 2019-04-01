f453f2a85cedbe9808714be7072cbcc72e4ecd7e
souravnandi42sn <36042925+souravnandi42sn@users.noreply.github.com>
2019-04-01 17:59:29 +0530

    Update calci.php

--- calci.php
+++ calci.php
@@ -4,39 +4,37 @@
 <script src="script.js"></script>
 </head>
 <body>
-	<div align="center">
+	<div align="center" class="calculator">
     <div class="display"><input type="text" readonly size="15.75" id="d" value="0" ></div>
-    <div class="keys">
         <p>
-        <input type="button" class="buttonld" id="myButton1" value="AC" onclick='display("")'>
-        <input type="button" class="buttonld" value="+/-" onclick='calculate("-")'>
-        <input type="button" class="buttonld" value="%" onclick='calculate("%")'>
-        <input type="button" class="buttonry" value="/" onclick='calculate("/")'>
+        <input type="button" class="buttonld" id="myButton1" value="AC" onclick='display("0")'><!--
+         --><input type="button" class="buttonld" value="+/-" onclick='calculate("-")'><!--
+         --><input type="button" class="buttonld" value="%" onclick='calculate("%")'><!--
+         --><input type="button" class="buttonry" value="/" onclick='calculate("/")'>
         </p>
         <p>
-        <input type="button" class="buttonll" value="7" onclick='calculate("7")'>
-        <input type="button" class="buttonll" value="8" onclick='calculate("8")'>
-        <input type="button" class="buttonll" value="9" onclick='calculate("9")'>
-        <input type="button" class="buttonry" value="*" onclick='calculate("*")'>
+        <input type="button" class="buttonll" value="7" onclick='calculate("7")'><!--
+         --><input type="button" class="buttonll" value="8" onclick='calculate("8")'><!--
+         --><input type="button" class="buttonll" value="9" onclick='calculate("9")'><!--
+         --><input type="button" class="buttonry" value="*" onclick='calculate("*")'>
         </p>
         <p>
-        <input type="button" class="buttonll" value="4" onclick='calculate("4")'>
-        <input type="button" class="buttonll" value="5" onclick='calculate("5")'>
-        <input type="button" class="buttonll" value="6" onclick='calculate("6")'>
-        <input type="button" class="buttonry" value="-" onclick='calculate("-")'>
+        <input type="button" class="buttonll" value="4" onclick='calculate("4")'><!--
+         --><input type="button" class="buttonll" value="5" onclick='calculate("5")'><!--
+         --><input type="button" class="buttonll" value="6" onclick='calculate("6")'><!--
+         --><input type="button" class="buttonry" value="-" onclick='calculate("-")'>
         </p>
         <p>
-        <input type="button" class="buttonll" value="1" onclick='calculate("1")'>
-        <input type="button" class="buttonll" value="2" onclick='calculate("2")'>
-        <input type="button" class="buttonll" value="3" onclick='calculate("3")'>
-        <input type="button" class="buttonry" value="+" onclick='calculate("+")'>
+        <input type="button" class="buttonll" value="1" onclick='calculate("1")'><!--
+         --><input type="button" class="buttonll" value="2" onclick='calculate("2")'><!--
+         --><input type="button" class="buttonll" value="3" onclick='calculate("3")'><!--
+         --><input type="button" class="buttonry" value="+" onclick='calculate("+")'>
         </p>
         <p>
-        <input type="button" class="buttonllw" value="0" onclick='calculate("0")'>
-        <input type="button" class="buttonll" value="." onclick='calculate(".")'>
-        <input type="button" class="buttonry" value="=" onclick='equal()'>
+        <input type="button" class="buttonllw" value="0" onclick='calculate("0")'><!--
+         --><input type="button" class="buttonll" value="." onclick='calculate(".")'><!--
+         --><input type="button" class="buttonry" value="=" onclick='equal()'>
         </p>
-    </div>
     </div>
 </body>
 </html>
